89626b9de36468ccbb84739784c137faa351f621
daniindosoft <danisaputraakatsuki@gmail.com>
2023-07-28 16:22:26 +0700

    fix gagal dadftar dan 404 error

--- error404.php
+++ error404.php
@@ -1,59 +1,79 @@
-<?php
-  include_once('dist/core/koneksi.php');
-  include_once('dist/core/system.php');
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="utf-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1">
+  
+  <link rel="shortcut icon" href="<?php echo $myDp; ?>">
+  
+  <title>Opps</title>
+<style>
+  /*======================
+    404 page
+=======================*/
 
-  $a = new koneksi();
-  $db = $a->hubungkan();
-  $onMy = new kontrols($db);
+.page_404 {
+  padding: 40px 0;
+  background: #fff;
+  font-family: "Arvo", serif;
+}
 
-  if ( $onMy->selectSingleOne('users', 'id', $_COOKIE['id_akun_combi'])['type_user'] == 'admin' ) {
-    require('header.php');
-  }else{
-    require('header_user.php');
-  }
-?>
-<div class="content-wrapper">
-  <!-- Content Header (Page header) -->
-  <div class="content-header">
-    <div class="container-fluid">
-      <div class="row mb-2">
-        <div class="col-sm-6">
-          <h1 class="m-0"></h1>
-        </div><!-- /.col -->
-        
-      </div><!-- /.row -->
-    </div><!-- /.container-fluid -->
-  </div>
-  <!-- /.content-header -->
+.page_404 img {
+  width: 100%;
+}
 
-  <?php
-    $type = array('file','vid')
-  ?>
-    
-  <section class="content">
-    <section class="content">
-      <div class="error-page">
-        <h2 class="headline text-warning"> 404</h2>
+.four_zero_four_bg {
+  background-image: url(https://cdn.dribbble.com/users/285475/screenshots/2083086/dribbble_1.gif);
+  height: 400px;
+  background-position: center;
+}
 
-        <div class="error-content">
-          <h3><i class="fas fa-exclamation-triangle text-warning"></i> Oops !</h3>
+.four_zero_four_bg h1 {
+  font-size: 80px;
+}
 
-          <p>
-            Halaman yang Anda cari tidak ditemukan, silahkan kembali ke <a href="<?php echo $onMy->primaryLocal ?>user/home">Beranda saja</a>.
-          </p>
+.four_zero_four_bg h3 {
+  font-size: 80px;
+}
 
-          
+.link_404 {
+  color: #fff !important;
+  padding: 10px 20px;
+  background: #39ac31;
+  margin: 20px 0;
+  display: inline-block;
+}
+.contant_box_404 {
+  margin-top: -50px;
+}
+
+</style>
+</head>
+<body>
+  
+
+<section class="page_404" style="text-align: center;">
+  <div class="container">
+    <div class="row">
+      <div class="col-sm-12 ">
+        <div class="col-sm-10 col-sm-offset-1  text-center">
+          <div class="four_zero_four_bg" >
+            <h1 class="text-center ">404</h1>
+
+          </div>
+
+          <div class="contant_box_404">
+            <h3 class="h2">
+              Opps, halaman yang di tuju tidak valid
+            </h3>
+
+            <p>Silahkan kembali atau ke halaman utama</p>
+
+            <a href="https://kombi.remotebisnis.com" class="btn btn-warning btn-sm" style="background-color:yellow; border: 2px solid black; padding:5px">Klik disini</a>
+          </div>
         </div>
-        <!-- /.error-content -->
       </div>
-      <!-- /.error-page -->
-    </section>
-  </section>
-  <!-- /.content -->
-</div> 
-  <script>
-    document.title = "KOMBI | Opps, Halaman tidak ditemukan !";
-  </script>
-<?php
-  require('footer.php');
-?>
+    </div>
+  </div>
+</section></body>
+</html>
